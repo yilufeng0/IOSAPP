c5e806e5e5cd848537d07d3cf88803052edd5b2b
li xiao <dev1@li-xiaomatoMac-mini.local>
2015-03-26 09:32:16 +0800

    create coreData init file

---
+++ CPTrace/CPTrace/CPSafeCoreData.h
@@ -0,0 +1,18 @@
+//
+//  CPSafe.h
+//  CPTrace
+//
+//  Created by li xiao on 15-3-26.
+//  Copyright (c) 2015年 buptLab618. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CPSafeAppDelegate.h"
+
+@interface CPSafeCoreData : NSObject
+
++(NSMutableArray*) getDataFromEntity:(NSString*)entityName;
+//+(BOOL) insertDataIntoEntity:(NSString*)entityName 
+
+
+@end
---
+++ CPTrace/CPTrace/CPSafeCoreData.m
@@ -0,0 +1,32 @@
+//
+//  CPSafe.m
+//  CPTrace
+//
+//  Created by li xiao on 15-3-26.
+//  Copyright (c) 2015年 buptLab618. All rights reserved.
+//
+
+#import "CPSafeCoreData.h"
+
+@implementation CPSafeCoreData
++(CPSafeAppDelegate*)appDelegate{
+    return [[UIApplication sharedApplication] delegate];
+}
+
++(NSMutableArray*) getDataFromEntity:(NSString*)entityName{
+    
+    NSManagedObjectContext* managementObjectContext=[[self appDelegate] managedObjectContext];
+    NSFetchRequest* request=[[NSFetchRequest alloc] init];
+    NSEntityDescription* entity=[NSEntityDescription entityForName:entityName inManagedObjectContext:managementObjectContext];
+    [request setEntity:entity];
+    
+    NSError* error=nil;
+    NSMutableArray* mutableFetchResults=[[managementObjectContext executeFetchRequest:request error:&error] mutableCopy];
+    if (mutableFetchResults == nil) {
+        NSLog(@"read fail");
+    }
+    return mutableFetchResults;
+}
+
+
+@end
